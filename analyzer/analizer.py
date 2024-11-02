ca17d25f54c67b0faaad22d0cdd998f8d5e00a17
Igor Polegato <igorpolegato@gmail.com>
2024-11-01 21:55:56 -0300

    Token type class

---
+++ analyzer/analizer.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+class TokenType:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return self.name
+
